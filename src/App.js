c89b4e362a1ac0989fd0ff8e34a2092362f37f2e
Kevin Claveria <kevin1793@gmail.com>
2023-08-06 21:44:13 -0500

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import logo from './assets/logo-64.png';
 import './App.css';
+import Sidebar from './components/sidebar';
+import Main from './components/main';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='appWrapper'>
+
+      <div className='wrapper'>
+        <Sidebar/>
+        <Main/>
+        {/* RightSide */}
+      </div>
     </div>
   );
 }
---
+++ src/components/sidebar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./sidebar.css";
+import HomeIcon from '@mui/icons-material/Home';
+
+function Sidebar(){
+  return(
+    <div className="sidebar">
+      <HomeIcon/>
+    </div>
+  )
+}
+
+export default Sidebar
